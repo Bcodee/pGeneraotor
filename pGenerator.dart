adb8a803cde98078184ad88a99fbd3df19728894
Bikram3309 <82156940+Bikram3309@users.noreply.github.com>
2022-04-27 19:56:08 +0900

    This is Password Generator Dart file

---
+++ pGenerator.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+  print("Enter Random Pass Length: ");
+  String? passLength = stdin.readLineSync();
+
+  try {
+    int myLength = int.parse(passLength!);
+    String randomPass = randomPassGenerator(length: myLength);
+    print(randomPass);
+  } catch (ex) {
+    print("Invalid Input");
+  }
+}
+
+// Create Random Password Generator Using [4 Min]
+String randomPassGenerator({int? length}) {
+  var alhpabets = ['a', 'b', 'c', 'd', 'e', 'f'];
+  var alphabetsC = ['A', 'B', 'C', 'D', 'E', 'F'];
+  var neumeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var symbols = ['_', '#', '%', '\$', ','];
+  String randomPassword = "";
+  var allitems = [...alhpabets, ...alphabetsC, ...neumeric, ...symbols];
+  allitems.shuffle();
+
+  for (int i = 0; i < length!; i++) {
+    if (length > allitems.length) {
+      print("Invalid Length");
+      break;
+    }
+    randomPassword += allitems[i].toString();
+  }
+  return randomPassword;
+}
